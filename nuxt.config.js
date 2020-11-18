3f221b560e5944786e585294274faa10df8e076a
Ali Can Türker <cantrker@gmail.com>
2020-11-18 14:00:48 +0300

    google analytics added

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
   css: ["kunodo/dist/kunodo.css"],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [],
+  plugins: [{ src: "~plugins/ga.js", mode: "client" }],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
